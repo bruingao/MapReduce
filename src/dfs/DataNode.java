165a68371c53bc800b8036b70d457c09b76579c5
bruingao <bruingao@gmail.com>
2013-11-13 21:46:11 -0500

    kvMerge

--- src/dfs/DataNode.java
+++ src/dfs/DataNode.java
@@ -14,181 +14,242 @@ import java.util.Random;
 
 import Common.Util;
 
+
+/**
+ * DataNode is the class running on a DataNode. It reads configuration
+ * files to get parameters and list of DataNode hostnames, creates local
+ * RMI registry and bind itself as a service. The DataNode is able to 
+ * recover from failures, read/write/remove local file chunks, make replica
+ * of file chunks, and respond to heartbeat status checks.
+ *
+ * @author      Rui Zhang
+ * @author      Jing Gao
+ * @version     1.0, 11/12/2013
+ * @since       1.0
+ */
 public class DataNode extends UnicastRemoteObject implements DataNodeI{
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2961863470847180775L;
-	
-	/* configuration file */
-	private static String confPath = "conf/dfs.conf";
-	
-	/* datanode file */
-	private static String dnPath = "conf/slaves";
-	
-	private static  Registry registry;
-	
-	/* stored files' replicas (filename + chunk number) */
-	private static volatile HashSet<String> files
-		= new HashSet<String>();
-	
-	/* datanodes' status */
-	private static volatile HashSet<String> datanodes 
-		= new HashSet<String>();
-	
-	/* all the files' path */
-	private static String dataNodePath;
-		
-	private static String registryHostname;
-	
-	private static Integer nameRegPort;
-	private static Integer dataRegPort;
-	
-	private static Integer dataNodePort;
-		
-	private static String dataNodeServiceName;
-	
-	private static String nameNodeServiceName;
-	
-	public DataNode() throws RemoteException{
-	
-	}
-	
-	private static void add_ts(HashSet<String> obj, String filename) {
-		synchronized(obj) {
-			files.add(filename);
-		}
-	}
-	
-	private static void remove_ts(HashSet<String> obj, String filename) {
-		synchronized(obj) {
-			files.remove(filename);
-		}
-	}
-	
-	/* init work */
-	private void Init() {
-		Object obj = Util.readObject(dataNodePath+"files");
-		if(obj != null)
-			files = (HashSet<String>) obj;
 
-	}
-	
-	/* read the corresponding chunk according to the filename */
-	@Override
-	public byte[] read(String filename) {
-		
-		String temp = dataNodePath + filename;
-		
-		return Util.readFromFile(temp);
-	}
-	
-	/* write the corresponding chunk */
-	@Override
-	public void write(String filename, byte[] content) {
-		
-		String temp = dataNodePath + filename;
-		
-		Util.writeBinaryToFile(content, temp);
-		
-		add_ts(files,filename);
-		
-		/* check point */
-		Util.writeObject(dataNodePath + "files", files);
-	}
-	
-	@Override
-	public void removeFile(String filename) throws RemoteException {
-				
-		try{
-    		File file = new File(dataNodePath+filename);
-    		file.setWritable(true);
-    		if(file.delete()){
-    			System.out.println(file.getName() + " is deleted!");
-    		}else{
-    			System.out.println("Delete operation is failed.");
-    		}
-    	}catch(Exception e){
-    		e.printStackTrace();
-    	}
+    private static final long serialVersionUID = 2961863470847180775L;
+    
+    /* configuration file */
+    private static String confPath = "conf/dfs.conf";
+    
+    /* DataNode hostname list file */
+    private static String dnPath = "conf/slaves";
+    
+    /* RMI registry on the DataNode */
+    private static Registry registry;
+    
+    /* set of local file replicas (filename + chunk number) */
+    private static volatile HashSet<String> files
+        = new HashSet<String>();
+    
+    /* set of DataNode hostnames */
+    private static volatile HashSet<String> datanodes 
+        = new HashSet<String>();
+    
+    /* paremeters read from the configuration file */
+    private static String dataNodePath;
+    private static String registryHostname;
+    private static Integer nameRegPort;
+    private static Integer dataRegPort;
+    private static Integer dataNodePort;
+    private static String dataNodeServiceName;
+    private static String nameNodeServiceName;
+    
+    /** 
+     * constructor of DataNode class
+     * 
+     * @since           1.0
+     */
+    public DataNode() throws RemoteException{
+    
+    }
+    
+    /** 
+     * thread-safely add a string to a hashset
+     * 
+     * @param obj       the hashset
+     * @param filename  the string to be added to the hashset
+     * @since           1.0
+     */
+    private static void add_ts(HashSet<String> obj, String filename) {
+        synchronized(obj) {
+            obj.add(filename);
+        }
+    }
+    
+    /** 
+     * thread-safely remove a string from a hashset
+     * 
+     * @param obj       the hashset
+     * @param filename  the string to be removed from the hashset
+     * @since           1.0
+     */
+    private static void remove_ts(HashSet<String> obj, String filename) {
+        synchronized(obj) {
+            obj.remove(filename);
+        }
+    }
+    
+    /** 
+     * recover the DataNode from local checkpoint
+     * 
+     * @since           1.0
+     */
+    private void Init() {
+        Object obj = Util.readObject(dataNodePath+"files");
+        if(obj != null)
+            files = (HashSet<String>) obj;
 
-		remove_ts(files,filename);
-		
-		/* check point */
-		Util.writeObject(dataNodePath + "files", files);
-	}
-
-	public static void readDataNodes(String filename) throws UnsupportedEncodingException {
-		String content = new String(Util.readFromFile(filename),"UTF-8");
-		
-		String lines[] = content.split("\n");
-		for(int i = 0; i < lines.length; i++) {
-			add_ts(datanodes,lines[i]);
-		}	
-	}
-	
-	@Override
-	public boolean heartBeat() {
-		return true;
-	}
-	
-	@Override
-	public boolean replication(String filename, String[] nodes)
-			throws RemoteException {
-		Date date = new Date();
-		Random rand = new Random(date.getTime());
-		
-		int index = rand.nextInt()%nodes.length;
-		
-		try {
-		
-		    Registry dnRegistry=LocateRegistry.getRegistry(nodes[index],dataRegPort);
-			DataNodeI datanode = (DataNodeI)dnRegistry.lookup(dataNodeServiceName);
-			byte[] content = datanode.read(filename);
-			this.write(filename, content);
-			return true;
-		} catch (NotBoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+    }
+    
+    /** 
+     * read the content of a file chunk
+     * 
+     * @param filename  the file name of the chunk to be read
+     * @return          the byte array of the file chunk
+     * @since           1.0
+     */
+    @Override
+    public byte[] read(String filename) {
+        String temp = dataNodePath + filename;
+        return Util.readFromFile(temp);
+    }
+    
+    /** 
+     * write the byte array to a file chunk
+     * 
+     * @param filename  the file name of the chunk
+     * @param content   the byte array to be written to file chunk
+     * @since           1.0
+     */
+    @Override
+    public void write(String filename, byte[] content) {
+        String temp = dataNodePath + filename;        
+        Util.writeBinaryToFile(content, temp);
+        
+        /* update local information */
+        add_ts(files,filename);
+        /* check point */
+        Util.writeObject(dataNodePath + "files", files);
+    }
+    
+    /** 
+     * remove a local file chunk, and then update local information
+     * and  checkpoint
+     * 
+     * @param filename  the file name of the chunk to be removed
+     * @since           1.0
+     */
+    @Override
+    public void removeFile(String filename) throws RemoteException {
+        
+        /* remove a local file chunk */
+        try{
+            File file = new File(dataNodePath+filename);
+            file.setWritable(true);
+            if(file.delete()){
+            
+            }else{
+                System.out.println("Delete operation is failed.");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        /* update local information */
+        remove_ts(files,filename);
+        /* check point */
+        Util.writeObject(dataNodePath + "files", files);
+    }
+    
+    /** 
+     * read the list of DataNode hostnames and store them
+     * 
+     * @param filename  the file name of DataNode list file
+     * @since           1.0
+     */
+    public static void readDataNodes(String filename) throws UnsupportedEncodingException {
+        String content = new String(Util.readFromFile(filename),"UTF-8");
+        
+        String lines[] = content.split("\n");
+        for(int i = 0; i < lines.length; i++) {
+            add_ts(datanodes,lines[i]);
+        }    
+    }
+    
+    /** 
+     * respond to status check from the NameNode
+     * 
+     * @return          true indicating DataNode is alive
+     * @since           1.0
+     */
+    @Override
+    public boolean heartBeat() {
+        return true;
+    }
+    
+    /** 
+     * make a replica of the specified file chunk from one of the residing DataNodes,
+     * and write the replica on local DataNode
+     * 
+     * @param filename  the file name of the chunk to be replicated
+     * @param nodes     the residing nodes
+     * @return          true if success, false otherwise
+     * @since           1.0
+     */
+    @Override
+    public boolean replication(String filename, String[] nodes) throws RemoteException {
+        /* randomly select one of the residing DataNodes from which to read */
+        Date date = new Date();
+        Random rand = new Random(date.getTime());
+        if(nodes.length <= 0)
 			return false;
-		}
-	}
-	
-	public static void main(String args[]) {
-		
-		try
-	    {
-			 DataNode datanode = new DataNode();
-			 Util.readConfigurationFile(confPath, datanode);
-			 readDataNodes(dnPath);
-			 datanode.Init();
+        int index = rand.nextInt()%nodes.length;
+        
+        try {
+            Registry dnRegistry=LocateRegistry.getRegistry(nodes[index],dataRegPort);
+            DataNodeI datanode = (DataNodeI)dnRegistry.lookup(dataNodeServiceName);
+            byte[] content = datanode.read(filename);
+            this.write(filename, content);
+            return true;
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    /** 
+     * The main function on the DataNode. It reads the configuration file
+     * and DataNode list, recovers from local checkpoint, creates a local 
+     * RMI registry and binds the DataNode on the registry as a service.
+     * 
+     * @param args      no arguments needed, parameters can be written in the configuration file
+     * @since           1.0
+     */
+    public static void main(String args[]) {
+        
+        try
+        {
+            DataNode datanode = new DataNode();
+            Util.readConfigurationFile(confPath, datanode);
+            readDataNodes(dnPath);
+            datanode.Init();
 
-			 unexportObject(datanode, false);
-			 DataNodeI stub = (DataNodeI) exportObject(datanode, dataNodePort);
-			 
-			 //registry = LocateRegistry.getRegistry(registryHostname, registryPort);
-			 registry = LocateRegistry.createRegistry(dataRegPort);
-			 //NameNodeI namenode = (NameNodeI)registry.lookup(registryHostname+"/"+nameNodeServiceName);
-			 
-			 InetAddress address = InetAddress.getLocalHost();
-			 
-			 System.out.println(address.getHostAddress());
-			 
-			 registry.rebind(dataNodeServiceName, stub);
-			 
-			 //System.out.println(dataNodeServiceName);
-			 //registry.rebind(dataNodeServiceName, stub);
-			 
-			 //System.out.println ("DataNode ready!");
-	    }
-	    catch (Exception e)
-	    {
-   	    	e.printStackTrace();
-
-	    	System.out.println("Exception happend when running the Datanode!");
-	    }
-		
-	}
+            unexportObject(datanode, false);
+            DataNodeI stub = (DataNodeI) exportObject(datanode, dataNodePort);
+            registry = LocateRegistry.createRegistry(dataRegPort);
+            InetAddress address = InetAddress.getLocalHost();
+            System.out.println(address.getHostAddress());
+            registry.rebind(dataNodeServiceName, stub);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("Exception happend when running the Datanode!");
+        }
+        
+    }
 
 }
--- src/dfs/NameNodeI.java
+++ src/dfs/NameNodeI.java
@@ -7,12 +7,38 @@ import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
 
 
+/**
+ * NameNodeI is the interface for NameNode. It defines interfaces to
+ * get the locations of chunks when opening file, get the locations of
+ * chunks when writing file to DFS, update the information and checkpoint
+ * after a write, list all files in DFS, list all DataNodes in DFS,
+ * remove file from DFS, check if a file exist in DFS.
+ *
+ * @author      Rui Zhang
+ * @author      Jing Gao
+ * @version     1.0, 11/12/2013
+ * @since       1.0
+ */
 public interface NameNodeI extends Remote{
-	public HashMap<Integer, HashSet<String>> open(String filename) throws RemoteException;
-	public HashMap<Integer, HashSet<String>> writeFile(String filename, int num) throws RemoteException;
-	public void writeSucess(String filename, boolean res) throws RemoteException;
-	public ConcurrentHashMap<String, HashMap<Integer, HashSet<String>>> listFiles() throws RemoteException;
-	public ConcurrentHashMap<String, HashSet<String>> listNodes() throws RemoteException;
-	public void removeFile(String filename) throws RemoteException;
-	public boolean checkname (String filename) throws RemoteException;
+    
+    /* pass the locations of chunks when opening a file in DFS */
+    public HashMap<Integer, HashSet<String>> open(String filename) throws RemoteException;
+    
+    /* pass the destination locations of chunks when writing a file to DFS */
+    public HashMap<Integer, HashSet<String>> writeFile(String filename, int num) throws RemoteException;
+    
+    /* update the information and checkpoint after a write */
+    public void writeSucess(String filename, boolean res) throws RemoteException;
+    
+    /* list all of the files stored on the DFS, along with locations of file chunks */
+    public ConcurrentHashMap<String, HashMap<Integer, HashSet<String>>> listFiles() throws RemoteException;
+    
+    /* list all of the DataNodes and the file chunks on the DataNodes */
+    public ConcurrentHashMap<String, HashSet<String>> listNodes() throws RemoteException;
+    
+    /* remove a file from DFS */
+    public void removeFile(String filename) throws RemoteException;
+    
+    /* check if a file exists in DFS */
+    public boolean checkname (String filename) throws RemoteException;
 }
