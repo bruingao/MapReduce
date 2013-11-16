c8674a580848b7da73000faade08fa2d00d03c9a
bruingao <bruingao@gmail.com>
2013-11-15 19:05:51 -0500

    another example

---
+++ src/sumVal/sumMapper.java
@@ -0,0 +1,15 @@
+package wordCount;
+
+import Common.Collector;
+import mapred.Mapper;
+
+public class sumMapper implements Mapper{
+
+	@Override
+	public void map(Object key, Object value, Collector collector) {
+		String newKey = (String)key;
+		int newValue = Integer.parseInt((String)value);
+		collector.collect(newKey, newValue);
+	}
+	
+}
---
+++ src/sumVal/sumVal.java
@@ -0,0 +1,29 @@
+package wordCount;
+
+import java.rmi.RemoteException;
+
+import format.inputFormatPair;
+import format.outputFormat;
+import mapred.JobClient;
+import mapred.JobConf;
+
+public class sumVal {
+	public static void main(String[] args) {
+		JobConf conf = new JobConf();
+		conf.setInputfile("inputSum");
+		conf.setOutputfile("outputSum.txt");
+		conf.setInputFormat(inputFormatPair.class);
+		conf.setOutputFormat(outputFormat.class);
+		conf.setMapperClass(sumMapper.class);
+		conf.setReducerClass(sumReducer.class);
+		
+		JobClient jobclient = new JobClient();
+		
+		try {
+			jobclient.runJob(conf);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
